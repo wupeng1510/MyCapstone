fe6f0174ed90c994975a993a8bcfa897a05fdeaf
wupeng1510 <wupeng1510@163.com>
2019-05-12 17:37:10 +0800

    update twist_controller.py

--- ros/src/twist_controller/twist_controller.py
+++ ros/src/twist_controller/twist_controller.py
@@ -49,7 +49,14 @@ class Controller(object):
         current_time=rospy.get_time()
         sample_time=current_time - self.last_time
         self.last_time=current_time
-
-        ############################################
-        #return 1., 0., 0.
-        self.controller=Controller()
+        
+        throttle=self.throttle_controller.step(vel_error,sample_time)
+        brake=0
+        if linear_vel==0. and current_vel<0.1:
+            throttle=0
+            brake=700
+        elif throttle <0.1 and vel_error<0:
+            throttle=0
+            decel=max(vel_error,self.decel_limit)
+            brake=abs(decel)*self.vehicle_mass*self.wheel_radius
+        return throttle,brake,steering
